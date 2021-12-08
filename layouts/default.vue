677f18e5383b8086dfae04a64752a875553708d6
sqrmyy <dn41.ra20adtdjw@gmail.com>
2021-12-08 20:06:20 +0900

    fix: Expected double colon

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <el-container>
+      <el-header height="60px"> TODO APP </el-header>
+      <nuxt />
+    </el-container>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 1.2rem;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.el-header {
+  background-color: #2cb696;
+  display: flex;
+  align-items: center;
+  color: white;
+}
+</style>
